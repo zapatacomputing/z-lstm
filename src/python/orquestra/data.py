e74d5707590178b11c1f7d176bc110c6d0f52ac1
AJ Roeth <aj.roeth@zapatacomputing.com>
2020-04-14 14:31:24 -0400

    Added debugging to function

--- src/python/orquestra/data.py
+++ src/python/orquestra/data.py
@@ -2,19 +2,32 @@
 This module generates a set of data.
 """
 
+import sys
 import numpy as np
 import pandas as pd
 
 def noisy_sine_generation(timerange=100, timestep=0.1, noisestd=0.2):
-  time = np.arange(0, timerange, timestep)
+  print(timerange)
+  print(type(timerange))
+  print(timestep)
+  print(type(timestep))
+  print(noisestd)
+  print(type(noisestd))
+  try:
+    time = np.arange(0, int(timerange), float(timestep))
+ 
+    # Generating data: sine function
+    data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
 
-  # Generating data: sine function
-  data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
+    # Testing numpy Array
+    print('Data shape from numpy: ', data.shape)
+    
+    # Testing pandas DataFrame
+    data_df = pd.DataFrame(data)
+    print('Data shape from pandas: ', data_df.shape)
+    print(data_df.head())
+  except:
+    e = sys.exc_info()[0]
+    print(e)
+    print('Something went wrong!')
 
-  # Testing numpy Array
-  print('Data shape from numpy: ', data.shape)
-  
-  # Testing pandas DataFrame
-  data_df = pd.DataFrame(data)
-  print('Data shape from pandas: ', data_df.shape)
-  print(data_df.head())

211598489be8f6277fb47ef834638da570bdecff
AJ Roeth <aj.roeth@zapatacomputing.com>
2020-04-14 14:02:46 -0400

    Added numsamples parameter

--- src/python/orquestra/data.py
+++ src/python/orquestra/data.py
@@ -5,16 +5,20 @@ This module generates a set of data.
 import numpy as np
 import pandas as pd
 
-def noisy_sine_generation():
-  # Generating data: sine function
+def noisy_sine_generation(numsamples):
   time = np.arange(0, 100, 0.1)
   noisestd = 0.2
-  data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
 
-  # Testing numpy Array
-  print('Data shape from numpy: ', data.shape)
-  
-  # Testing pandas DataFrame
-  data_df = pd.DataFrame(data)
-  print('Data shape from pandas: ', data_df.shape)
-  print(data_df.head())
+  for i in range(1, numsamples+1):
+    print('Sample ', i)
+
+    # Generating data: sine function
+    data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
+
+    # Testing numpy Array
+    print('Data shape from numpy: ', data.shape)
+    
+    # Testing pandas DataFrame
+    data_df = pd.DataFrame(data)
+    print('Data shape from pandas: ', data_df.shape)
+    print(data_df.head())

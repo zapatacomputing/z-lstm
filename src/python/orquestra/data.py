cacddd5a34eb02389682fa8275ccf7fba44804e7
AJ Roeth <aj.roeth@zapatacomputing.com>
2020-04-14 14:20:53 -0400

    Adding different input params to generate data func

--- src/python/orquestra/data.py
+++ src/python/orquestra/data.py
@@ -5,20 +5,16 @@ This module generates a set of data.
 import numpy as np
 import pandas as pd
 
-def noisy_sine_generation(numsamples):
-  time = np.arange(0, 100, 0.1)
-  noisestd = 0.2
+def noisy_sine_generation(timerange=100, timestep=0.1, noisestd=0.2):
+  time = np.arange(0, timerange, timestep)
 
-  for i in range(1, numsamples+1):
-    print('Sample ', i)
+  # Generating data: sine function
+  data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
 
-    # Generating data: sine function
-    data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
-
-    # Testing numpy Array
-    print('Data shape from numpy: ', data.shape)
-    
-    # Testing pandas DataFrame
-    data_df = pd.DataFrame(data)
-    print('Data shape from pandas: ', data_df.shape)
-    print(data_df.head())
+  # Testing numpy Array
+  print('Data shape from numpy: ', data.shape)
+  
+  # Testing pandas DataFrame
+  data_df = pd.DataFrame(data)
+  print('Data shape from pandas: ', data_df.shape)
+  print(data_df.head())

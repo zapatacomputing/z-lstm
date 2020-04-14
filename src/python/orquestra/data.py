4df0e1770d3f105eaa517555af619d2ce9556b0c
AJ Roeth <aj.roeth@zapatacomputing.com>
2020-04-14 14:41:28 -0400

    Bug fix

--- src/python/orquestra/data.py
+++ src/python/orquestra/data.py
@@ -7,25 +7,23 @@ import numpy as np
 import pandas as pd
 
 def noisy_sine_generation(timerange=100, timestep=0.1, noisestd=0.2):
-  print(timerange)
-  print(type(timerange))
-  print(timestep)
-  print(type(timestep))
-  print(noisestd)
-  print(type(noisestd))
+  # timerange = int(timerange)
+  # timestep = float(timestep)
+  # noisestd = float(noisestd)
+
   try:
-    time = np.arange(0, int(timerange), float(timestep))
+    time = np.arange(0, timerange, timestep)
  
     # Generating data: sine function
-    data = np.sin(time) + np.random.normal(scale=float(noisestd), size=len(time))
+    data = np.sin(time) + np.random.normal(scale=noisestd, size=len(time))
 
     # Testing numpy Array
     print('Data shape from numpy: ', data.shape)
     
     # Testing pandas DataFrame
-    data_df = pd.DataFrame(data)
+    data_df = pd.DataFrame(data, columns=['Values'])
     print('Data shape from pandas: ', data_df.shape)
-    print(data_df.head())
+    print(data_df.head(3))
   except:
     e = sys.exc_info()[0]
     print(e)

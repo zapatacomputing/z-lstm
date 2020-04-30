7a1f89e71b3b5adfa8e49342c0a7852867f001d3
Francisco Javier Fernandez Alcazar <javier.alcazar@zapatacomputing.com>
2020-04-30 17:07:43 -0400

    [#172419733] Moved all data functions into data.py

--- src/python/orquestra/data.py
+++ src/python/orquestra/data.py
@@ -5,6 +5,8 @@ This module generates a set of data.
 import sys
 import numpy as np
 import pandas as pd
+import json
+from typing import TextIO
 
 def noisy_sine_generation(time_range, time_step, noise_std) -> dict:
   print('time_range = ', time_range)
@@ -39,3 +41,71 @@ def noisy_sine_generation(time_range, time_step, noise_std) -> dict:
 
   return data_dict
 
+def preprocess_data(data, train_perc = 0.8, window_size = 10):
+  # Load data into dataframe
+  df = pd.DataFrame.from_dict(data)
+  print("DataFrame head:")
+  print(df.head())
+  
+  dfsize = df.shape[0]
+
+  # Splitting up dataset into Training and Testing datsets
+  train_size = int(dfsize * train_perc)
+  test_size = dfsize - train_size
+  train, test = df.iloc[0:train_size], df.iloc[train_size:]
+
+  print("Train and test set sizes")
+  print(len(train), len(test))
+
+  # Reshape to dimensions required by tensorflow: [samples, window_size, n_features]
+  col = df.columns[1]
+  train_windows, train_next_vals = create_dataset(train[col], train[col], window_size)
+  test_windows, test_next_vals = create_dataset(test[col], test[col], window_size)
+
+  # Save all 4 data sets to dictionaries
+  train_dict = {}
+  train_dict["data"] = train.to_dict()
+
+  test_dict = {}
+  test_dict["data"] = test.to_dict()
+
+  train_window_dict = {"data":{}}
+  train_window_dict["data"]["windows"] = train_windows.tolist()
+  train_window_dict["data"]["next_vals"] = train_next_vals.tolist()
+
+  test_window_dict = {"data":{}}
+  test_window_dict["data"]["windows"] = test_windows.tolist()
+  test_window_dict["data"]["next_vals"] = test_next_vals.tolist()
+
+  return train_dict, test_dict, train_window_dict, test_window_dict
+
+def create_dataset(x, y, window_size=1):
+  xs, ys = [], []
+  for i in range(len(x) - window_size):
+      v = x.iloc[i:(i + window_size)].values
+      xs.append(v)
+      ys.append(y.iloc[i + window_size])
+
+  return np.array(xs), np.array(ys)
+
+def save_data(datas: list, filenames: list) -> None:
+  for i in range(len(datas)):
+    data = datas[i]
+    filename = filenames[i]
+    
+    print("Saving this data:\n", data)
+
+    data["schema"] = "orquestra-v1-data"
+
+    with open(filename,'w') as f:
+      f.write(json.dumps(data, indent=2)) # Write data to file as this will serve as output artifact
+
+
+def load_data(file: TextIO) -> dict:
+  if isinstance(file, str):
+    with open(file, 'r') as f:
+      data = json.load(f)
+  else:
+    data = json.load(file)
+
+  return data
--- src/python/orquestra/data_test.py
+++ src/python/orquestra/data_test.py
@@ -1,6 +1,10 @@
 import unittest
-import json
 from data import *
+import json
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import os.path
 
 class TestDataGeneration(unittest.TestCase):
   def test_noisy_sine_generation(self):
@@ -13,6 +17,107 @@ class TestDataGeneration(unittest.TestCase):
     self.assertEqual(len(data_dict["data"]["values"]), time_range/time_step)
     last_index = list(data_dict["data"]["time"].keys())[-1]
     self.assertAlmostEqual(data_dict["data"]["time"][last_index], time_range-time_step)
+
+  def test_preprocess_data(self):
+    
+    train_perc = 0.8
+    window_size = 10
+
+    test_file_path = Path("test/test_data.json")
+    with open(test_file_path) as test_file:
+      test_data = json.load(test_file)
+
+    data = preprocess_data(test_data, train_perc, window_size)
+    train_dict = data[0]
+    test_dict = data[1]
+    train_window_dict = data[2]
+    test_window_dict = data[3]
+
+    self.assertEqual(len(train_dict["data"]["time"]), 400)
+    self.assertEqual(len(test_dict["data"]["time"]), 100)
+    self.assertEqual(len(train_window_dict["data"]["windows"]), 390)
+    self.assertEqual(len(test_window_dict["data"]["next_vals"]), 90)
+    
+  def test_create_dataset(self):
+    x = [1,2,3,4]
+    y = [5,6,7,8]
+    time_steps = 2
+
+    df = pd.DataFrame(data=np.transpose([x, y]), columns=['x','y'])
+
+    xx, yy = create_dataset(df.y, df.y, time_steps)
+
+    self.assertTrue((xx == [[5,6],[6,7]]).all())
+    self.assertTrue((yy == [7, 8]).all())
+    
+  def test_save_data(self):
+    data_1 = {"data":
+      {
+        "time": {
+          "0": 0,
+          "1": 1
+        },
+        "vals": {
+          "0": 2.0,
+          "1": 3.0
+        }
+      }
+    }
+    expected_artifact_1 = data_1
+    expected_artifact_1["schema"] = "orquestra-v1-data"
+
+    data_2 = {"data":
+      {
+        "time": {
+          "0": 0,
+          "1": 1
+        },
+        "vals": {
+          "0": 4.0,
+          "1": 5.0
+        }
+      }
+    }
+    expected_artifact_2 = data_2
+    expected_artifact_2["schema"] = "orquestra-v1-data"
+
+    datas = [data_1, data_2]
+    filenames = ['data_1.json','data_2.json']
+    expected_artifacts = [expected_artifact_1, expected_artifact_2]
+    save_data(datas, filenames)
+
+    for i in range(len(filenames)):
+      self.assertTrue(os.path.isfile(filenames[i]))
+
+      with open(filenames[i], 'r') as f:
+        artifact = json.load(f)
+
+      self.assertDictEqual(artifact, expected_artifacts[i])
+
+      try:
+        os.remove(filenames[i])
+      except OSError:
+        pass
+  
+  def test_load_data(self):
+    expected_data = {
+      "data":
+      {
+        "time": {
+          "0": 0,
+          "1": 1
+        },
+        "vals": {
+          "0": 2.0,
+          "1": 3.0
+        }
+      },
+      "schema": "orquestra-v1-data"
+    }
+
+    data = load_data("test/test_data_artifact.json")
+
+    self.assertDictEqual(data, expected_data)
 
 if __name__ == '__main__':
   unittest.main()
